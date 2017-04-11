8c4ee730421f034717f8226c42568da56614d821
Aaron English <anotherasitis@users.noreply.github.com>
2017-04-11 17:03:22 -0400

    Added the connection between the add crystal button and the formation of a dockable window. Likely going to have to switch to a more generic widget setup instead of the docks to simplify connection and parameter handoff. This is also now ready to merge back into the add dock branch.

--- graphBase.py
+++ graphBase.py
@@ -77,16 +77,41 @@ class graphBase(QtGui.QWidget):
 		# self.newCrystalBut.clicked.connect(self.addCrystalParam)
 		# self.splitterSubAdd.addWidget(self.newCrystalBut)
 
-		# # add a subsplitter to allow for many crystals to be visualized
-		# self.splitterSubView = QtGui.QSplitter()
-		# self.splitterSubView.setOrientation(QtCore.Qt.Vertical)
-		# self.splitterMain.addWidget(self.splitterSubView)
+		# add a subsplitter to allow for many crystals to be visualized
+		self.splitterSubView = QtGui.QSplitter()
+		self.splitterSubView.setOrientation(QtCore.Qt.Vertical)
+		self.splitterMain.addWidget(self.splitterSubView)
 
 		
 	# Adds a news window to add a new crystal next door
 	def addCrystalParam(self):
 		# newView = crystalParamBase.crystalParamBase()
 		self.crystalBase.addCrystalView(self.params)
+		
+		self.area = DockArea()
+		self.splitterSubView.addWidget(self.area)
+
+		# self.d1 = Dock("Dock1", size = (1, 1)) 
+		self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
+		# self.area.addDock(self.d1, 'left')
+		self.area.addDock(self.d2, 'left') 
+
+		self.w = gl.GLViewWidget()
+		self.w.setBackgroundColor('k')
+		self.w.setWindowTitle('Structure')
+		self.w.setCameraPosition(distance = 3, azimuth = -280)
+		self.d2.addWidget(self.w)
+
+		# self.subArea =  DockArea() # give this dock the minimum possible size
+		# self.subD1 = Dock("Sub Dock 1", size = (1, 1))  
+		# self.subD2 = Dock("Sub Dock 2", size = (1, 1))
+
+		# self.subArea.addDock(self.subD1, 'top') 
+		# self.subArea.addDock(self.subD2, )
+		# self.radio1 = QtGui.QRadioButton()
+		# self.radio1.setText("Radio 1")
+		# self.d1.addWidget(self.radio1,row = 0,col = 0)
+		# self.d1.addWidget(self.subArea,row = 0,col = 1)
 
 
 		
@@ -121,30 +146,7 @@ class crystalViewBase(pTypes.GroupParameter):
 			])
 
 		pTypes.GroupParameter.__init__(self, **defs)
-		# self.area = DockArea()
-		# graphBaseWin.splitterSubView.addWidget(self.area)
-
-		# self.d1 = Dock("Dock1", size = (1, 1)) 
-		# self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
-		# self.area.addDock(self.d1, 'left')
-		# self.area.addDock(self.d2, 'left') 
-
-		# self.w = gl.GLViewWidget()
-		# self.w.setBackgroundColor('k')
-		# self.w.setWindowTitle('Structure')
-		# self.w.setCameraPosition(distance = 3, azimuth = -280)
-		# self.d2.addWidget(self.w)
-
-		# self.subArea =  DockArea() # give this dock the minimum possible size
-		# self.subD1 = Dock("Sub Dock 1", size = (1, 1))  
-		# self.subD2 = Dock("Sub Dock 2", size = (1, 1))
-
-		# self.subArea.addDock(self.subD1, 'top') 
-		# self.subArea.addDock(self.subD2, )
-		# self.radio1 = QtGui.QRadioButton()
-		# self.radio1.setText("Radio 1")
-		# self.d1.addWidget(self.radio1,row = 0,col = 0)
-		# self.d1.addWidget(self.subArea,row = 0,col = 1)
+		
 
 class graphsToShow(pTypes.GroupParameter):
 
