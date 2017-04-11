b7e6a9b0775b6d40c3201cfe518175dfda7c85a0
Aaron English <anotherasitis@users.noreply.github.com>
2017-04-11 16:50:26 -0400

    This commit goes back on the claims of the previous one. This version of graphBase has all the associated classes in the same file. It is able to create the layout with multiple crystals and has check boxes for displaying different data. Turns out all the classes in on place makes it easy to keep track of everything.

--- crystalParamBase.py
+++ crystalParamBase.py
@@ -10,17 +10,50 @@ import pyqtgraph.opengl as gl
 
 class crystalParamBase(QtGui.QWidget):
 
-	def __init__(self, graphBaseWin):
-		self.splitterSubSubParam = QtGui.QSplitter()
-		self.splitterSubSubParam.setOrientation(QtCore.Qt.Vertical)
+	def __init__(self):
 		self.tree = ParameterTree(showHeader=False)
-		self.newCrystalView = QtGui.QPushButton('Show Crystal Structure', self)
-		self.newCrystalView.clicked.connect(self.addCrystalView)
-		self.splitterSubSubParam.addWidget(self.tree)
-		self.splitterSubSubParam.addWidget(self.newCrystalView)
-		graphBaseWin.splitterSubAdd.addWidget(self.splitterSubSubParam)
+
+		self.viewParams = Parameter.create(name='Cyrstal and View Parameters', type='group',
+			children=[dict(name='Chemical Formula', type='str', value='', readonly=True),
+			dict(name='Polytype', type='str', value='', readonly=True),
+			dict(name='Temperature (in K)', type='float', value='', readonly=True),
+			dict(name='Pressure (in kPa)', type='float', value='', readonly=True),
+			dict(name='Add Crystal', type='action'),
+			])
+
+		self.tree.setParameters(self.params, showTop=False)
+		self.params.param('Add Crystal').sigActivated.connect(self.addCrystalParam)
+
+		# self.newCrystalView = QtGui.QPushButton('Show Crystal Structure', self)
+		# self.newCrystalView.clicked.connect(self.addCrystalView)
+		# self.splitterSubSubParam.addWidget(self.tree)
+		# self.splitterSubSubParam.addWidget(self.newCrystalView)
+		# graphBaseWin.splitterSubAdd.addWidget(self.splitterSubSubParam)
 
 		self.crystalView=[]
 
 	def addCrystalView(self):
-		self.crystalView.append(crystalViewBase.CrystalViewBase(grapBaseWin, self))
+		self.area = DockArea()
+		graphBaseWin.splitterSubView.addWidget(self.area)
+
+		self.d1 = Dock("Dock1", size=(1, 1)) 
+		self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
+		self.area.addDock(self.d1, 'left')
+		self.area.addDock(self.d2, 'left') 
+
+		self.w = gl.GLViewWidget()
+		self.w.setBackgroundColor('k')
+		self.w.setWindowTitle('Structure')
+		self.w.setCameraPosition(distance=3, azimuth=-280)
+		self.d2.addWidget(self.w)
+
+		self.subArea =  DockArea() # give this dock the minimum possible size
+		self.subD1 = Dock("Sub Dock 1", size=(1, 1))  
+		self.subD2 = Dock("Sub Dock 2", size=(1, 1))
+
+		self.subArea.addDock(self.subD1, 'top') 
+		self.subArea.addDock(self.subD2, )
+		self.radio1 = QtGui.QRadioButton()
+		self.radio1.setText("Radio 1")
+		self.d1.addWidget(self.radio1,row=0,col=0)
+		self.d1.addWidget(self.subArea,row=0,col=1)
--- graphBase.py
+++ graphBase.py
@@ -1,7 +1,7 @@
 ################################################################################
 # This File contains multiple classes that all require one another. Original 
 # attemps to break it in to many different files for each class seem misguided. 
-# In time however, this may prove to be unwise.
+# In time however, maybe this will prove unwise.
 ################################################################################
 
 from pyqtgraph.dockarea import *
@@ -26,22 +26,28 @@ class graphBase(QtGui.QWidget):
 	def __init__(self):
 		QtGui.QWidget.__init__(self)
 
-		# self.crystalViewer=[]
+		# self.crystalViewer = []
 
 		self.setUpGui()
 
+		self.crystalBase = crystalParamBase()
+
 		# self.crystalParamGroup = crystalParamBase()
-		self.params = Parameter.create(name='params', type='group',
-			children=[dict(name='Chemical Formula', type='str', value=''),
-			dict(name='Temperature (in K)', type='float', value=293),
-			dict(name='Pressure (in kPa)', type='float', value=101),
-			dict(name='Add Crystal', type='action'),
+		self.params = Parameter.create(name = 'params', type = 'group',
+			children = [dict(name = 'Chemical Formula', type = 'str', value = '', default = ''),
+			dict(name = 'Polytype', type = 'str', value = '', default = ''),
+			dict(name = 'Temperature', type = 'float', value = 293, default = 293, 
+				siPrefix = True, suffix = 'K'),
+
+			dict(name = 'Pressure', type = 'float', value = 101, default = 101, 
+				siPrefix = True, suffix = 'kPa'),
+
+			dict(name = 'Add Crystal', type = 'action'),
+			self.crystalBase,
 			])
 
-		self.tree.setParameters(self.params, showTop=False)
+		self.tree.setParameters(self.params, showTop = False)
 		self.params.param('Add Crystal').sigActivated.connect(self.addCrystalParam)
-
-
 
 
 	def setUpGui(self):
@@ -61,7 +67,7 @@ class graphBase(QtGui.QWidget):
 		self.splitterSubAdd.setOrientation(QtCore.Qt.Vertical)
 		self.splitterMain.addWidget(self.splitterSubAdd)
 		
-		self.tree = ParameterTree(showHeader=False)
+		self.tree = ParameterTree(showHeader = False)
 		self.splitterSubAdd.addWidget(self.tree)
 
 
@@ -79,50 +85,100 @@ class graphBase(QtGui.QWidget):
 		
 	# Adds a news window to add a new crystal next door
 	def addCrystalParam(self):
-		print('done')
+		# newView = crystalParamBase.crystalParamBase()
+		self.crystalBase.addCrystalView(self.params)
+
+
 		
 
-# class crystalParamBase(pTypes.GroupParameter):
+class crystalParamBase(pTypes.GroupParameter):
 
-# 	def __init__(self, **kwds):
-# 		self.splitterSubSubParam = QtGui.QSplitter()
-# 		self.splitterSubSubParam.setOrientation(QtCore.Qt.Vertical)
-# 		self.tree = ParameterTree(showHeader=False)
-# 		self.newCrystalView = QtGui.QPushButton('Show Crystal Structure', self)
-# 		self.newCrystalView.clicked.connect(self.addCrystalView)
-# 		self.splitterSubSubParam.addWidget(self.tree)
-# 		self.splitterSubSubParam.addWidget(self.newCrystalView)
-# 		graphBaseWin.splitterSubAdd.addWidget(self.splitterSubSubParam)
+	def __init__(self, **kwds):
+		pTypes.GroupParameter.__init__(self, name = 'Crystals')
 
-# 		self.crystalView=[]
+	def addCrystalView(self, paramsToApply):
+		self.addChild(crystalViewBase(paramsToApply))
+		
 
-# 	def addCrystalView(self):
-# 		self.crystalView.append(crystalViewBase.CrystalViewBase(grapBaseWin, self))
+class crystalViewBase(pTypes.GroupParameter):
 
-# class crystalViewBase(QtGui.QWidget):
+	def __init__(self, paramsToApply):
 
-# 	def __init__(self, graphBaseWin, crystalParams):
-# 		self.area = DockArea()
-# 		graphBaseWin.splitterSubView.addWidget(self.area)
+		self.graphsToShow = graphsToShow()
 
-# 		self.d1 = Dock("Dock1", size=(1, 1)) 
-# 		self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
-# 		self.area.addDock(self.d1, 'left')
-# 		self.area.addDock(self.d2, 'left') 
+		defs = dict(name = paramsToApply.param('Chemical Formula').value(),
+			removable = True, children = [dict(name = 'Polytype', type = 'str', 
+				value = paramsToApply.param('Polytype').value(), readonly = True),
 
-# 		self.w = gl.GLViewWidget()
-# 		self.w.setBackgroundColor('k')
-# 		self.w.setWindowTitle('Structure')
-# 		self.w.setCameraPosition(distance=3, azimuth=-280)
-# 		self.d2.addWidget(self.w)
+			dict(name = 'Temperature', type = 'float', 
+				value = paramsToApply.param('Temperature').value(), 
+				readonly = True, siPrefix = True, suffix = 'K'),
 
-# 		self.subArea =  DockArea() # give this dock the minimum possible size
-# 		self.subD1 = Dock("Sub Dock 1", size=(1, 1))  
-# 		self.subD2 = Dock("Sub Dock 2", size=(1, 1))
+			dict(name = 'Pressure', type = 'float',
+				value = paramsToApply.param('Pressure').value(),
+				readonly = True, siPrefix = True, suffix = 'kPa'),
+			self.graphsToShow,
+			])
 
-# 		self.subArea.addDock(self.subD1, 'top') 
-# 		self.subArea.addDock(self.subD2, )
-# 		self.radio1 = QtGui.QRadioButton()
-# 		self.radio1.setText("Radio 1")
-# 		self.d1.addWidget(self.radio1,row=0,col=0)
-# 		self.d1.addWidget(self.subArea,row=0,col=1)
+		pTypes.GroupParameter.__init__(self, **defs)
+		# self.area = DockArea()
+		# graphBaseWin.splitterSubView.addWidget(self.area)
+
+		# self.d1 = Dock("Dock1", size = (1, 1)) 
+		# self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
+		# self.area.addDock(self.d1, 'left')
+		# self.area.addDock(self.d2, 'left') 
+
+		# self.w = gl.GLViewWidget()
+		# self.w.setBackgroundColor('k')
+		# self.w.setWindowTitle('Structure')
+		# self.w.setCameraPosition(distance = 3, azimuth = -280)
+		# self.d2.addWidget(self.w)
+
+		# self.subArea =  DockArea() # give this dock the minimum possible size
+		# self.subD1 = Dock("Sub Dock 1", size = (1, 1))  
+		# self.subD2 = Dock("Sub Dock 2", size = (1, 1))
+
+		# self.subArea.addDock(self.subD1, 'top') 
+		# self.subArea.addDock(self.subD2, )
+		# self.radio1 = QtGui.QRadioButton()
+		# self.radio1.setText("Radio 1")
+		# self.d1.addWidget(self.radio1,row = 0,col = 0)
+		# self.d1.addWidget(self.subArea,row = 0,col = 1)
+
+class graphsToShow(pTypes.GroupParameter):
+
+	def __init__(self):
+		 defs = dict(name = 'Display...', children = [
+		 	dict(name = 'Crystal Lattice', type = 'bool', value = False, default = False,
+		 		children = [
+		 		dict(name = 'Crystal Dimension to Display (X)', type = 'float', value = 1,
+		 			default = 1),
+
+		 		dict(name = 'Crystal Dimension to Display (Y)', type = 'float', value = 1,
+		 			default = 1),
+
+		 		dict(name = 'Crystal Dimension to Display (Z)', type = 'float', value = 1,
+		 			default = 1)
+		 		]),
+
+		 	dict(name = 'Reciprocal Lattice', type = 'bool', value = False, default = False,
+		 		children = [
+		 		dict(name = 'Brillouin Zones to Show', type = 'int', value = 0, default = 0)
+		 		]),
+
+		 	dict(name = 'Show Planes', type = 'bool', value = False, default = False,
+		 		children = [
+		 		dict(name = 'Axis', type = 'int', value = 000, default = 000) 
+		 		]),
+
+		 	dict(name = 'X-Ray Diffraction Pattern', type = 'bool', value = False, default = False,
+		 		children = [
+		 		dict(name = 'Axis', type = 'int', value = 000, default = 000) 
+		 			]),
+
+		 	dict(name = 'Phonon Dispersion Curve', type = 'bool', value = False, default = False),
+		 	dict(name = 'Electronic Band Structure', type = 'bool', value = False, default = False),
+		 	])
+
+		 pTypes.GroupParameter.__init__(self, **defs)
