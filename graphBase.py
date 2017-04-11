6627bba5dae79b30fc4c8c8034ac847d39c5b424
Aaron English <anotherasitis@users.noreply.github.com>
2017-04-10 22:56:02 -0400

    Further advancement of the UI, implementation of buttons in graphBase. Approaching ability to handle multiple crystal structure views at once. Also added comments about project dependancies in graphTop and comments explaining functions in graphBase

--- graphBase.py
+++ graphBase.py
@@ -1,37 +1,64 @@
-from pyqtgraph.Qt import QtCore, QtGui
-import pyqtgraph as pg
-import pyqtgraph.opengl as gl
-import numpy as np
-import itertools as itTl
-import makeBox as mkBx
-import makeGrids as mkGds
-import makeCrystalStruct as mkXtlSt
-import makeOcthedron as mkOct
-import makePlanes as mkPlns
-import pyqtgraph.exporters
 from pyqtgraph.dockarea import *
-from pyqtgraph.parametertree import Parameter, ParameterTree
+from pyqtgraph.Qt import QtCore, QtGui
 from pyqtgraph.parametertree import types as pTypes
+from pyqtgraph.parametertree import Parameter, ParameterTree
+import pyqtgraph as pg
+import pyqtgraph.exporters
+import pyqtgraph.opengl as gl
+# import numpy as np
+# import makeBox as mkBx
+# import itertools as itTl
+# import makeGrids as mkGds
+# import makePlanes as mkPlns
+# import makeOcthedron as mkOct
+# import makeCrystalStruct as mkXtlSt
+
 
 class graphBase(QtGui.QWidget):
 	
 	def __init__(self):
 		QtGui.QWidget.__init__(self)
 
+		# Insert a layout into the main window where other widgets can be added
 		self.layout = QtGui.QVBoxLayout()
 		self.layout.setContentsMargins(0,0,0,0)
 		self.setLayout(self.layout)
 
-		self.splitter = QtGui.QSplitter()
-		self.splitter.setOrientation(QtCore.Qt.Vertical)
+		# Insert a widget that allows for division of the layout
+		self.splitterMain = QtGui.QSplitter()
+		self.splitterMain.setOrientation(QtCore.Qt.Horizontal)
+		self.layout.addWidget(self.splitterMain)
 
-		self.layout.addWidget(self.splitter)
-		
+		# View for adding crystals
+		self.splitterSubAdd = QtGui.QSplitter()
+		self.splitterSubAdd.setOrientation(QtCore.Qt.Vertical)
+		self.splitterMain.addWidget(self.splitterSubAdd)
+
+		# Button for adding new crystal
+		self.newCrystalBut = QtGui.QPushButton('Add a New Crystal', self)
+		self.newCrystalBut.clicked.connect(self.addCrystalParam)
+		self.splitterSubAdd.addWidget(self.newCrystalBut)
+
+		# add a subsplitter to allow for many crystals to be visualized
+		self.splitterSubView = QtGui.QSplitter()
+		self.splitterSubView.setOrientation(QtCore.Qt.Vertical)
+		self.splitterMain.addWidget(self.splitterSubView)
+
+	# Adds a news window to add a new crystal next door
+	def addCrystalParam(self):
+		self.splitterSubSubParam = QtGui.QSplitter()
+		self.splitterSubSubParam.setOrientation(QtCore.Qt.Vertical)
 		self.tree = ParameterTree(showHeader=False)
-		self.splitter.addWidget(self.tree)
+		self.newCrystalView = QtGui.QPushButton('Show Crystal Structure', self)
+		self.newCrystalView.clicked.connect(self.addCrystalView)
+		self.splitterSubSubParam.addWidget(self.tree)
+		self.splitterSubSubParam.addWidget(self.newCrystalView)
+		self.splitterSubAdd.addWidget(self.splitterSubSubParam)
 
+	# 
+	def addCrystalView(self):
 		self.area = DockArea()
-		self.splitter.addWidget(self.area)
+		self.splitterSubView.addWidget(self.area)
 
 		self.d1 = Dock("Dock1", size=(1, 1)) 
 		self.d2 = Dock('Dock2')    ## give this dock the minimum possible size
@@ -54,6 +81,3 @@ class graphBase(QtGui.QWidget):
 		self.radio1.setText("Radio 1")
 		self.d1.addWidget(self.radio1,row=0,col=0)
 		self.d1.addWidget(self.subArea,row=0,col=1)
-
-		if self.radio1.isChecked():
-			print('Hellobo')
--- graphTop.py
+++ graphTop.py
@@ -1,6 +1,16 @@
-import pyqtgraph as pg
+###################################################################
+# This project is built on pyqtgraph v 0.10.0 and so at time of 
+# development, one of the key components is unfinished. As such
+# (and because it's just generally good practice) great effort 
+# has been made to completely isolate all the physics stuff from 
+# the display stuff. Ideally, it should be feasable to completely
+# replace the graphics and plotting with another library with 
+# little effort. 
+###################################################################
+
 from pyqtgraph.Qt import QtCore, QtGui
 import graphBase
+import pyqtgraph as pg
 
 if __name__ == '__main__':
 	import sys
