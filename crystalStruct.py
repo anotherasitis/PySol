e1691370f20b57d3bbf222877ba3afb3624fd263
Aaron English <anotherasitis@users.noreply.github.com>
2017-04-18 22:10:51 -0400

    Adding the crystal class for proper cronstruction of the structure.

---
+++ crystalStruct.py
@@ -0,0 +1,13 @@
+from scipy import spatial
+import numpy as np
+import itertools as itTl
+
+class crystalStruct():
+
+	def __init__(self):
+		self.atms
+		self.polyType
+		self.primLatVex
+		self.latPnts
+		self.latVecConnect
+		self.
