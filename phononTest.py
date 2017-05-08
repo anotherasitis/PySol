c07e0fb29be57fc70ed8d5ca601c9af9d7da124a
Aaron English <anotherasitis@users.noreply.github.com>
2017-05-08 10:41:39 -0400

    incomplete early phonon dispersion curve implementation. can be merged without problem.

--- phononTest.py
+++ phononTest.py
@@ -14,6 +14,26 @@ xpre = np.array([0,1,0,0,1,1,0,1,0.5,0.5,1,0.5,0,0.5,0.25,0.75,0.25,0.75])
 ypre = np.array([0,0,1,0,1,0,1,1,0.5,0.5,0.5,1,0.5,0,0.25,0.75,0.75,0.25])
 zpre = np.array([0,0,0,1,0,1,1,1,0,1,0.5,0.5,0.5,0.5,0.25,0.25,0.75,0.75])
 
+displacmentA = np.array([0.02,0.02,0])
+
+frcCnstSrt = 2
+frcCnstBnd = 4
+
+m1 = 1
+m2 = 1.25
+
+at1 = np.array([0.25,0.25,0.25])
+at1pp = np.array([0.5,0.5,0])
+at1mm = np.array([0,0,0])
+at1mp = np.array([0,0.5,0.5])
+at1pm = np.array([0.5,0,0.5])
+
+at2 = np.array([0,0,0])
+at2pp = np.array([0.25,0.25,0.25])
+at2mm = np.array([-0.25,-0.25,0.25])
+at2mp = np.array([-0.25,0.25,-0.25])
+at2pm = np.array([0.25,-0.25,-0.25])
+
 symPts = dict(
 	gamma = scl*np.add(org,np.array([bxSdLn*0.5,bxSdLn*0.5,bxSdLn*0.5])),
 	L = scl*np.add(org,np.array([bxSdLn*0.5+(a*np.sqrt(2)/2),bxSdLn*0.5+(a*np.sqrt(2)/2),
@@ -32,3 +52,6 @@ for i in symLnComb:
 	for indx, j in enumerate(a):
 		symLns[i].append(np.linspace(j[0], j[1], num = resolutionForPho))
 	symLns[i] = np.array(symLns[i])
+
+forceMat = np.empty([6,6])
+forceMat[0,0] = 
